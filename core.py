f8ea4a6fc9a5398bdb4ec2aea2c0a5de1a69a672
Róbert Kéri <rkeri95@gmail.com>
2020-06-22 14:42:21 +0200

    add basic tings

--- core.py
+++ core.py
@@ -1,1 +1,18 @@
-# blank 1
+import tkinter as tk
+import os
+import youtube_dl
+import tkinter.font as tkfont
+from tkinter import *
+from tkinter.ttk import *
+
+#i need dat window
+root = tk.Tk()
+
+pic = PhotoImage(file = "qqq.pgm")
+root.iconphoto(False, pic)
+
+root.title("Youtube DL")
+root.geometry("600x600")
+#root.resizable(width = FALSE, height = FALSE)
+
+root.mainloop()
